82bfc839ed7b9f39605eed14a6a8109404ef0574
CsAdi <oradam86@gmail.com>
2023-08-18 15:02:22 +0200

    Ignore dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'something/something.dart';
+import 'ignore.dart';
 
 extension Log on Object {
   void log() => devtools.log(toString());
--- lib/something/something.dart
+++
@@ -1,2 +0,0 @@
-const String something1 = 'http://asd4:5500/api/persons1.json';
-const String something2 = 'http://asd4:5500/api/persons2.json';
