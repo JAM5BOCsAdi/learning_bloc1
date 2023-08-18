dd2ef226cd5d5336dd1aeaf4887ae04a00164485
CsAdi <oradam86@gmail.com>
2023-08-18 14:32:18 +0200

    somethingv2

--- lib/something/something.dart
+++ lib/something/something.dart
@@ -1,2 +1,2 @@
-const String something1 = 'http://asd:5500/api/persons1.json';
-const String something2 = 'http://asd:5500/api/persons2.json';
+const String something1 = 'http://asd2:5500/api/persons1.json';
+const String something2 = 'http://asd2:5500/api/persons2.json';
