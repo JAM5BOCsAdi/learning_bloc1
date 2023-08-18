8a74692504bb54f63700174b4875138a971b7681
CsAdi <oradam86@gmail.com>
2023-08-18 15:14:49 +0200

    Ignore dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'ignore.dart';
+import '../ignore.dart';
 
 extension Log on Object {
   void log() => devtools.log(toString());
